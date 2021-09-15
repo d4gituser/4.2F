fb71f8995a14cbab0f8344a0b21afe89c9e3ac12
Durjoy <72721406+d4gituser@users.noreply.github.com>
2021-09-16 03:34:22 +0600

    mid last push
    lel

--- CSE_426_Lab/Lab 6/main.cpp
+++ CSE_426_Lab/Lab 6/main.cpp
@@ -9,7 +9,7 @@ float angle= 45;
 
 
 void spin(void){
-    angle = angle+0.1;//increasing the additional value will speedup rotation.
+    angle = angle+0.1;//increasing the addition value will speedup rotation.
     glutPostRedisplay();
 }
 void display(void){
@@ -17,10 +17,10 @@ void display(void){
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     gluLookAt(0,0,7,0,0,0,0,1,0);
-    glRotatef(angle,1,0,0);
-    glRotatef(angle,0,1,0);
-    glRotatef(angle,0,0,1);
-    glutWireCube(2);
+    glRotatef(angle,1,0,0);//rotate with x axis
+    glRotatef(angle,0,1,0);//rotate with y axis
+    glRotatef(angle,0,0,1);//rotate with z axis
+    glutWireCube(2);//There are many shapes of glutWire here we use cube and its size is 2.
     glutSwapBuffers(); //works like glflush but its used for 3d objects.
 
  }
@@ -37,7 +37,7 @@ void reshape(int w, int h){
 int main(int argc, char **argv){
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_DOUBLE);//In 2D we used glut_single but for 3D we need to be dynamic thus using glut_double
     glutInitWindowSize(800,800);
     glutInitWindowPosition(100,100);
     glutCreateWindow("rashik");
